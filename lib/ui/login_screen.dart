95c07683bf673705c46d924adfa9ca67fc2e4b64
Hassan Asqiriba <federiz@msn.com>
2020-10-24 14:16:49 -0700

    intro animation hotfix

--- lib/ui/login_screen.dart
+++ lib/ui/login_screen.dart
@@ -3,7 +3,14 @@ import 'dart:async';
 import 'package:flutter_login/flutter_login.dart';
 import 'main_screen.dart';
 
+const users = const {
+  'e@e.e': 'eee',
+  'user@mail.com': 'pass',
+};
+
 class LoginScreen extends StatelessWidget {
+  Duration get loginTime => Duration(milliseconds: 2250);
+
   @override
   Widget build(BuildContext context) {
     Future nextPage() {
@@ -14,37 +21,50 @@ class LoginScreen extends StatelessWidget {
       });
     }
 
+    Future<String> _authUser(LoginData data) {
+      print('Name: ${data.name}, Password: ${data.password}');
+      return Future.delayed(loginTime).then((_) {
+        if (!users.containsKey(data.name)) {
+          return 'Username not exists';
+        }
+        if (users[data.name] != data.password) {
+          return 'Password does not match';
+        }
+        return null;
+      });
+    }
+
     return Scaffold(
-      floatingActionButton: FloatingActionButton.extended(
-        label: Text(
-          'About us',
-          style: TextStyle(
-            color: Colors.white,
-          ),
-        ),
-        icon: Icon(
-          Icons.assignment_ind,
-          color: Colors.white,
-        ),
-        backgroundColor: Colors.deepPurpleAccent,
-        onPressed: () {
-          showModalBottomSheet(
-              context: context,
-              isScrollControlled: true,
-              builder: (context) => SingleChildScrollView(
-                    child: Container(
-                      padding: EdgeInsets.only(
-                          bottom: MediaQuery.of(context).viewInsets.bottom),
-                      child: SizedBox(),
-                    ),
-                  ));
-        },
-      ),
+//      floatingActionButton: FloatingActionButton.extended(
+//        label: Text(
+//          'About us',
+//          style: TextStyle(
+//            color: Colors.white,
+//          ),
+//        ),
+//        icon: Icon(
+//          Icons.assignment_ind,
+//          color: Colors.white,
+//        ),
+//        backgroundColor: Colors.deepPurpleAccent,
+//        onPressed: () {
+//          showModalBottomSheet(
+//              context: context,
+//              isScrollControlled: true,
+//              builder: (context) => SingleChildScrollView(
+//                    child: Container(
+//                      padding: EdgeInsets.only(
+//                          bottom: MediaQuery.of(context).viewInsets.bottom),
+//                      child: SizedBox(),
+//                    ),
+//                  ));
+//        },
+//      ),
       body: FlutterLogin(
         title: 'ColNet',
         logo: 'assets/images/logo.png',
-        onLogin: (_) => nextPage(), //Implement login function here.
-        onSignup: (_) => nextPage(), //Implement signup function here.
+        onLogin: _authUser, //Implement login function here.
+        onSignup: _authUser, //Implement signup function here.
         onSubmitAnimationCompleted: () {
           Navigator.of(context).pushReplacement(MaterialPageRoute(
             builder: (context) => MainScreen(),
